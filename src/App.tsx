75746ffbebe9420c74ee4b293162891035082dd8
muslimumarov <norbekumarov613@gmail.com>
2025-07-18 17:42:23 +0500

    boshlandi

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Navbar from "./layouts/base/navbar/Navbar.tsx";
+
+const App: React.FC = () => {
+
+    return (
+      <div className="min-h-screen">
+        <Navbar />
+        <h1>boshlandi</h1>
+      </div>
+    );
+  }
+
+
+export default App;
